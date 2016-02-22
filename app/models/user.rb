4c0c0aaa5d0faa9d9200d9207b1a865e931c0b95
sunshengying <sunshengying@yeah.net>
2016-02-22 09:05:28 +0800

    Refactor the user_controller.rb & user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,34 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def sign_in!(session)
+    session[:id] = id
     session[:name] = name
+  end
+  
+  def sign_out!(session)
+    session[:id] = nil
+    session[:name] = nil
   end
   
   def clear_password!
     self.password = nil
+  end
+
+  def remember!(cookies)
+    cookies_expiration = 1.months.from_now
+    cookies[:remember_me] = { value: 1, expires: cookies_expiration }
+    self.authorization_token = unique_identifier
+    save!
+    cookies[:authorization_token] = { value: authorization_token, expires: cookies_expiration }
+  end
+
+  def forget!(cookies)
+    cookies.delete(:remember_me)
+    cookies.delete(:authorization_token)
+  end
+
+private
+  def unique_identifier
+    Digest::SHA1.hexdigest("#{name}:#{password_digest}")
   end
 end
