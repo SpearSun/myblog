dd96933bc4755386e8328e13d7131c9c1ba6553f
Sun Shengying <sunshengying@yeah.net>
2016-02-29 09:49:16 +0800

    Test user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,44 @@
 require 'rails_helper'
 
 describe User do
-  before do
+  before(:each) do
     @user = User.new
+    @user.id = 1
+    @user.name = 'test'
     @user.password = 'password'
     @user.clear_password!
   end
 
+  context '.sign_in!' do
+    session = {}
+    it do
+      @user.sign_in!(session)
+      session[:id] = @user.id
+      session[:name] = @user.name
+    end
+  end
+
+  context '.sign_out!' do
+    session = { id: 1, name: 'test' }
+    it do
+      @user.sign_out!(session)
+      session[:id] = nil
+      session[:name] = nil
+    end
+  end
 
   context '.clear_password!' do
-
     it "password was reset as nil" do
-      expect(@user.password).to eq('password')
+      expect(@user.clear_password!).to eq(nil)
     end
   end
   
+  context '.forget!' do
+    cookies = { remember_me: 1, authorization_token: 'test' }
+    it do
+      @user.forget!(cookies)
+      cookies[:remember_me] = nil
+      cookies[:authorization_token] = nil
+    end
+  end
 end
